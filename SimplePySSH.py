048ebfa4559be41f0b2c3fb5ed5539fd892aad95
Robert Percival <robert@mactechs.com>
2015-12-31 12:09:29 -0800

    Added ability to set key authorization for future ssh sessions

--- SimplePySSH.py
+++ SimplePySSH.py
@@ -6,10 +6,14 @@
 
 import getpass
 import os
+import platform
 import pty
 import re
-import sys
 import socket
+import subprocess
+import sys
+
+LocalOS = platform.system()
 
 class SSHError(Exception):
     def __init__(self, value):
@@ -26,8 +30,12 @@ class SSH:
     def run_cmd(self, c):
         (pid, f) = pty.fork()
         if pid == 0:
-            os.execlp("/usr/bin/ssh", "ssh",
-                "-t", self.user + '@' + self.ip, c)
+            if c.startswith("sudo"):
+                os.execlp("/usr/bin/ssh", "ssh",
+                    "-t", self.user + '@' + self.ip, c)
+            else:
+                os.execlp("/usr/bin/ssh", "ssh",
+                    self.user + '@' + self.ip, c)
         else:
             return (pid, f)
 
@@ -83,6 +91,23 @@ class SSH:
         (pid, f) = self.run_cmd(c)
         return self.ssh_results(pid, f)
 
+    def set_key_auth(self, user):
+        RemoteOS = self.cmd("uname").strip()
+        if RemoteOS == 'Darwin':
+            ssh_dir = "/private/var/root/.ssh"
+        elif RemoteOS == 'Linux':
+            ssh_dir = "/root/.ssh"
+        else:
+            raise ValueError('Unsupported OS on remote machine: %s' % RemoteOS)
+        auth_keys = ssh_dir + "/authorized_keys"
+        pub_key = ssh_keygen(user)
+        cmd = "sudo mkdir -p %s" % ssh_dir
+        self.cmd(cmd)
+        cmd = "sudo touch %s" % auth_keys
+        self.cmd(cmd)
+        cmd = "sudo echo \"%s\" | sudo tee -a %s" % (pub_key, auth_keys)
+        self.cmd(cmd)
+
 def ssh_cmd(ip, user, passwd, cmd):
     s = SSH(ip, user, passwd)
     return s.cmd(cmd)
@@ -102,19 +127,35 @@ def get_ip():
         ip = raw_input("Enter ip address of target machine: ")
     return ip
 
-# def ssh_keygen():
-#     id_rsa = "/Users/%s/.ssh/id_rsa" % user
-#     if os.path.isfile(id_rsa):
-#         # gen key from pre-existing id_rsa
-#     else:
-#         # need to gen id_rsa priv key
+def get_local_user():
+    if os.environ.has_key('SUDO_USER'):
+        user = os.environ['SUDO_USER']
+    else:
+        user = os.environ['USER']
+    return user
+
+def ssh_keygen(user):
+    if LocalOS == 'Darwin':
+        homedir = "/Users/"
+    elif LocalOS == 'Linux':
+        homedir = "/home/"
+    else:
+        raise ValueError('Unsupported OS on local machine: %s' % LocalOS)
+    id_rsa = homedir + "%s/.ssh/id_rsa" % user
+    if os.path.isfile(id_rsa):
+        pub_key = subprocess.check_output(["ssh-keygen", "-y", "-f", id_rsa]).strip()
+    else:
+        pub_key = subprocess.check_output(["ssh-keygen", "-t", "rsa", "-N", '', "-f", id_rsa]).strip()
+    return pub_key
 
 
 if __name__ == "__main__":
+    local_user = get_local_user()
     ip = get_ip()
     user = raw_input("Enter target username: ")
     passwd = getpass.getpass(prompt="Enter the password for the target user: ")
     ssh = SSH(ip, user, passwd)
+    #ssh.set_key_auth(local_user)
     while True:
         cmd = raw_input("Enter the command you wish to run on remote machine: ")
         if cmd == "exit()":
